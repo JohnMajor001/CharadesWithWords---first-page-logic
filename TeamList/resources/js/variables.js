247877837b58854a1f803ceb1cc18fdbf763c9b2
John Major <john@orbonline.co.uk>
2018-04-09 17:00:15 +0100

    stuff

--- TeamList/resources/js/variables.js
+++ TeamList/resources/js/variables.js
@@ -12,5 +12,7 @@ var teamNamesArray = [];
 // Things to do!
 /* Make Team Name Box bigger
       Put 'X' at side of each third player box to deleteBtn
-      do the same for team actually
-      
+      do the same for team actually*/
+
+// If you add teams, delete some, then add some - identical ids end up being created
+// making some with identical ids as a result then it all goes to poop
