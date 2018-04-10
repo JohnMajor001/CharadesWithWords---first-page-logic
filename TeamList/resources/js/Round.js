a333b7d2fdc9cf864dcbfa0cbf8e3c41d07c0a00
John Major <john@orbonline.co.uk>
2018-04-10 16:59:21 +0100

    See notes in round

--- TeamList/resources/js/Round.js
+++ TeamList/resources/js/Round.js
@@ -6,7 +6,9 @@
 
 function roundPrep(team) {
   // readyBtn.addEventListener('click', roundBegin);
-  //var heading = document.createElement('h1');
-  //var body = ``;
+  // var heading = document.createElement('h1');
+  // var body = `<p>${team.name}, it's your round.</p>
+  //             <p>${team.players[team.whoseGo]} you're describing.</p>`;
+  //
   alert(team.name);
 }
